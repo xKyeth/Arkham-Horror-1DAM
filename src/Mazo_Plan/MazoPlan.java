c3fc09e7682d8b0a8b42139340ea93731ddd046b
imanol <imanol@DAM6>
2019-02-19 14:07:46 +0100

    He añadido dos metodos nuevos en el mazo del plan, uno que crea las cartas del plan y otro que esta en construccion que dara informacion de que hacen las cartas en detalle.

---
+++ src/Mazo_Plan/MazoPlan.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Mazo_Plan;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author imanol
+ */
+public class MazoPlan {
+    /*
+                    INSTRUCCIONES DEL MAZO DEL PLAN
+    
+    -El mazo del plan representa el avance de los malos
+    -Si se invoca una resolucion(generalmente mas siniestra)los jugadores pierden el escenario
+    -Para ver como acaba la resolucion leer la seccion "No leas esto hasta el final del escenario"
+    -Si se esta jugando una campaña se pasa al siguiente escenario con las consecuencias negativas
+    de haber fallado la resolucion
+    -El mazo avanza si hay una cantidad sufienciente de "perdicion", las cartas del plan cuestan perdicion  
+    sí el plan tiene una instruccion objetivo anula el req. de perdicion o añade req. adiccionales
+    -Para avanzar el mazo del plan: 
+        1-retirar toda la perdicion de las cartas que haya en juego
+        2-Dar la vuelta a la carta que se esta haciendo avanzar y seguir sus instrucciones
+        3-Cuando se elije nueva carta de plan se descarta la que esta en juego    
+    */
+        //Declaracion de varibales
+        private int opcion;
+        Scanner sc = new Scanner(System.in); 
+            
+        //Lista donde se guardan las cartas del plan
+        LinkedList<CartaPlan1> cartasPlan1 = new LinkedList<>();
+        LinkedList<CartaPlan2> cartasPlan2 = new LinkedList<>();
+        LinkedList<CartaPlan3> cartasPlan3 = new LinkedList<>();
+        LinkedList<CartaPlan4> cartasPlan4 = new LinkedList<>();
+        LinkedList<CartaPlan5> cartasPlan5 = new LinkedList<>();
+        LinkedList<CartaPlan6> cartasPlan6 = new LinkedList<>();
+        LinkedList<CartaPlan7> cartasPlan7 = new LinkedList<>();
+        LinkedList<CartaPlan8> cartasPlan8 = new LinkedList<>();
+        
+        //Este metodo añade las cartas del plan a las listas.
+        public void añadirCartasPlan(){
+            
+            cartasPlan1.add(new CartaPlan1("¡¿Qué está pasando?!"));
+            cartasPlan2.add(new CartaPlan2("El asceso de los gules"));
+            cartasPlan3.add(new CartaPlan3("¡Están saliendo!"));
+            cartasPlan4.add(new CartaPlan4("¿Depredador o presa?"));
+            cartasPlan5.add(new CartaPlan5("El tiempo se agota"));
+            cartasPlan6.add(new CartaPlan6("The Arkham Woods"));
+            cartasPlan7.add(new CartaPlan7("El ritual comienza"));
+            cartasPlan8.add(new CartaPlan8("La venganza espera"));
+
+    }
+
+        //Este metodo segun la carta que se seleccione se dara la informacion que hace dicha carta
+        public void infoCartasPlan(){
+        
+            
+            
+            opcion = sc.nextInt();
+            switch(opcion){
+                
+            }
+            
+            
+            
+            
+            
+        }
+    
+            
+        
+    
+}
