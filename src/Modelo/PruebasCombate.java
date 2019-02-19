aa0a373bbac22803633f054c2afd1867dbdaae9a
miguel <miguel@DAM5>
2019-02-19 12:44:54 +0100

    Creadas las pruebas de combate, necesito en la clase Investigador y Enemigo los atributos que he descrito en la clase.

--- src/Modelo/PruebasCombate.java
+++ src/Modelo/PruebasCombate.java
@@ -5,10 +5,60 @@
  */
 package Modelo;
 
+import Modelo.Cartas.Carta;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  *
  * @author miguel
  */
 public class PruebasCombate {
+    /**Me falta el enemigo y el investigador, he basado 
+     esta parte asumiendo que en los atributos de cada uno
+     se van a añadir (es importante) los atributos:
+     salud(int),valorCombate(int),modificadorDaño(int).
+     Tambien es importante crear para la clase Enemigo y Investigador
+     un enum para los estados VIVO y MUERTO (lo podriamos llamar estado o integridadPersonaje)
+     ahora mismo tengo controlados los estados con true o false de forma
+     que si hacemos lo del enum pueda modificarse facilmente
+     pero la otra forma me parece mas eficiente**/
+    
+    Investigador investigador;
+    Enemigo enemigo;
+    LinkedList<Enemigo> pilaVictoria;
+    public PruebasCombate() {
+        this.investigador= new Investigador();
+        this.pilaVictoria=new LinkedList<Enemigo>();
+    }
+    public void nuevoEnemigo(){
+        this.enemigo=new Enemigo();
+    }
+    
+    
+    public void enfrenta(){
+        if(investigador.valorCombate>=enemigo.valorCombate){
+            enemigo.salud=enemigo.salud-(1+investigador.modificadorDaño);
+        }else{
+            investigador.salud=investigador.salud-enemigo.daño;
+        }
+    }
+    public boolean muerteEnemigo(){
+        boolean muerto=false;
+        if(enemigo.salud<=0){
+            muerto=true;
+            if(enemigo.puntosDeVictoria){
+                pilaVictoria.push(enemigo);
+            }
+        }
+        return muerto;
+    }
+    public boolean muerteInvestigador(){
+        boolean muerto=false;
+        if(investigador.salud<=0){
+            muerto=true;
+        }
+        return muerto;
+    }
     
 }
