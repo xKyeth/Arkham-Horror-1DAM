dfe116dfb934d07f6c4fa11eb6642449cffc9b33
usuario <usuario@equipo-12>
2019-02-20 09:03:34 +0100

    Implementacion de los valores de las cartas de acto

--- src/Modelo/Cartas/Cartas_Acto/TheBarrier.java
+++ src/Modelo/Cartas/Cartas_Acto/TheBarrier.java
@@ -5,10 +5,29 @@
  */
 package Modelo.Cartas.Cartas_Acto;
 
+import Modelo.PruebasCombate;
+
 /**
  *
  * @author alumno
  */
 public class TheBarrier extends Carta_Acto {
+    public TheBarrier() {
+        coste=3;
+        add=false;
+        titulo="La barrera";
+        pretexto="Una brillante barrera os bloquea el paso hacia la salita. Al acercaros, un intenso calor os obliga a retroceder. Cogéis algo de tierra y la lanzáis contra la barrera, donde para vuestro horror queda totalmente incinerada. Puede que en el sótano o el ático haya algo que os sirva de ayuda.";
+        accion="La barrera que bloqueaba el paso hacia la Salita se ha desvanecido. Revela la Salita.\n" +
+"\n" +
+"Pon en juego en la Salita la carta Lita Chantler que habías puesto aparte.\n" +
+"\n" +
+"Haz aparecer en el Pasillo el Sacerdote gul que habías puesto aparte.";
+        objetivo="Cuando acabe la ronda, los investigadores que estén en el Pasillo pueden, como grupo, gastar la cantidad necesaria de pistas para hacer avanzar el acto.";
+        texto="Usáis el barril del ático para transportar hielo y nieve del sótano y lanzarlo contra la barrera. Ésta chisporrotea y vibra al consumir el hielo, para después desvanecerse con un siseo.";
+    }
+    @Override
+    public boolean usarCarta(int pistas, Object obj, PruebasCombate pr) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
