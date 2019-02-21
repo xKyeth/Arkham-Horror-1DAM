bc06a13c8f789443e5566909129c60bbede5cfac
usuario <usuario@localhost>
2019-02-21 09:24:13 +0100

    Control del mazo inicial

--- src/Controlador/EvitarEnemigo.java
+++ src/Controlador/EvitarEnemigo.java
@@ -15,13 +15,12 @@ public class EvitarEnemigo {
   public EvitarEnemigo(){}
   
   
-  public boolean EvitarEnemigo(int discrep, int bonif, int percep){
-  
-  int tot=discrep+bonif-percep;
+  public boolean EvitarEnemigo(int discre, int bonif, int percep){
+
+  int tot=discre+bonif-percep;
 for (int x=tot;x>0;x--) {
 int dado = (int) (Math.random() * 6) + 1;
 if (dado==6 || dado==5){
     return true;
 }
-}return false;
-}}
+}return false;}}
