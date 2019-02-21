c943fc14fe29262a202e1c576102742b36fd33b0
usuario <usuario@localhost>
2019-02-21 09:24:27 +0100

    Control del mazo inicial

---
+++ src/Controlador/ControlGeneral.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+/**
+ *
+ * @author usuario
+ */
+public class ControlGeneral {
+    
+    public ControlGeneral(){
+    
+    
+    }
+    
+    public void Juego(){
+    
+    
+    
+    
+    }
+    
+    
+    
+    public void prepararPartida(){}
+    
+    
+    
+    public void faseMitos(){
+    
+    
+    
+    }
+    
+    public void faseInvestigacion(){
+    
+    //Robar 1 carta
+    
+    //Obtener un recurso
+    
+    //Acivar una capacidad
+    
+    //Enfrentarse a un enemigo
+    
+    //Investigar
+    
+    //Moverse
+    
+    //Jugar una carta de apoyo o Evento de su mano
+    
+    //intentar evitar a un enemigo
+    
+    //Combatir con un enemigo
+    
+    }
+    
+    public void faseEnemigos(){
+    
+    //Mover a monstruos Cazador
+    
+    
+    //Enemigo ataca 
+    
+    
+    
+    }
+    
+    public void faseMantenimiento(){
+    
+    //Poner boca arriba carta pequeña
+    
+    //Prepara todas las cartas agotadas
+        
+    //robar un carta y obtiene un recurso
+    
+    //si hay mas de 8 cartas elige y descarta 
+    
+    }
+}
