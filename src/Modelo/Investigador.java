a7b7a531b6bbcab5d7faaf3894562f1c67ac8626
usuario <usuario@PC-112>
2019-02-25 11:56:51 +0100

    Subo los métodos para conseguir recursos

--- src/Modelo/Investigador.java
+++ src/Modelo/Investigador.java
@@ -1,13 +1,15 @@
 package Modelo;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public abstract class Investigador extends Personaje{
     
     protected String tipo;
-    protected  int voluntad,intelecto,comodin,acciones,numeroPistas,numeroRecursos;
+    protected  int voluntad,intelecto,comodin,acciones,numeroPistas;
     protected  String habilidadPropia;
     protected  MazoInvestigador mazo;
+    protected int numeroRecursos=5;
 
     abstract int  efectoEstrella();
     
@@ -15,6 +17,35 @@ public abstract class Investigador extends Personaje{
        Object lugar=lugares.get(posicion);
        personaje.setLugar(lugar);
     }
+     
+     public void getRecursoFase() {
+         
+         numeroRecursos=numeroRecursos+1;
+         System.out.println("Tienes "+numeroRecursos+" recursos");
+     }
+     
+     public void getRecursoRobo() {
+         
+         numeroRecursos=numeroRecursos+1;
+         System.out.println("Tienes "+numeroRecursos+" recursos");
+     }
+     
+     public void getRecursoEfecto() {
+         
+         System.out.println("¿Cuántos recursos tienes que obtener?");
+         Scanner sq=new Scanner(System.in);
+         int numero=sq.nextInt();
+         int contador=0;
+         while (contador<numero){
+             
+             numeroRecursos=numeroRecursos+1;
+             contador++;
+         }
+         
+         System.out.println("Tienes "+numeroRecursos+" recursos");
+     }
+     
+     
     
 
   public Investigador(){
