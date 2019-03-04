e8a92b687b2a46938ce19c498f92e1b899f70b95
Miguel <Miguel@192.168.1.3>
2019-03-04 09:38:35 +0100

    Añadida la prueba de combate (comentada) a modelohabilidad, me faltan los enemigos que no se ahora mismo de donde sacarlos, lo demás creo que todo ok

--- src/modelohabilidad/PruebaCombate.java
+++ src/modelohabilidad/PruebaCombate.java
@@ -7,6 +7,7 @@ package modelohabilidad;
 
 import BolsaDelCaos.BolsaDelCaos;
 import Modelo.RolandBanks;
+import java.util.LinkedList;
 
 /**
  *
@@ -17,6 +18,55 @@ public class PruebaCombate extends PruebaHabilidad{
     public PruebaCombate(int n, RolandBanks inv, Apoyo apoyo, BolsaDelCaos bolsa) {
         super(n, inv, apoyo, bolsa);
     }
+        /**Me falta el enemigo y el investigador, he basado 
+     esta parte asumiendo que en los atributos de cada uno
+     se van a añadir (es importante) los atributos:
+     salud(int),valorCombate(int),modificadorDaño(int).
+     Tambien es importante crear para la clase Enemigo y Investigador
+     un enum para los estados VIVO y MUERTO (lo podriamos llamar estado o integridadPersonaje)
+     ahora mismo tengo controlados los estados con true o false de forma
+     que si hacemos lo del enum pueda modificarse facilmente
+     pero la otra forma me parece mas eficiente**/
+    
+    Investigador investigador;
+//    Enemigo enemigo;
+//    LinkedList<Enemigo> pilaVictoria;
+    
+//    public void nuevoEnemigo(){
+//        this.enemigo=new Enemigo();
+//    }
+    
+    
+//    public void enfrenta(){
+//        int vidainv=inv.getVida();
+//        int vidaene=inv.getVida();
+//        if(inv.getDaño()>=enemigo.getDaño){
+//            enemigo.getVida()=enemigo.getVida()-(1+inv.getDaño());
+//        }else{
+//            vidainv=vidainv-enemigo.getDaño;
+//        }
+//    }
+//    public boolean muerteEnemigo(){
+//        boolean muerto=false;
+//        if(enemigo.getVida()<=0){
+//            muerto=true;
+//            /**if(Enemigo tiene puntos de victoria){
+//                pilaVictoria.push(enemigo);
+//            }**/
+//        }
+//        return muerto;
+//    }
+//    public boolean muerteInvestigador(){
+//        boolean muerto=false;
+//        if(inv.getVida()<=0){
+//            muerto=true;
+//        }
+//        return muerto;
+//    }
+
+//    public LinkedList<Enemigo> getPilaVictoria() {
+//        return pilaVictoria;
+//    }
 
     @Override
     public void iniciarPruebaVoluntad() {
