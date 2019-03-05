6495cf6d25591517ef1e7ebe02dda620d95dc231
imanol <imanol@DAM6>
2019-03-05 13:33:49 +0100

    Destreza manual añadida

---
+++ src/Modelo/DestrezaManual.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+import Modelo.Cartas.Mazo;
+import modelohabilidad.PruebaHabilidad;
+/**
+ *
+ * @author imanol
+ */
+public class DestrezaManual {
+    
+    PruebaHabilidad ph;
+    Mazo m;
+    public void DestrezaManual(){
+        ph.iniciarPrueba(0);
+        //Si la prueba tiene exito se roba una carta
+        //if(prueba tiene exito)
+        m.cogerCarta();
+    }
+    
+}
