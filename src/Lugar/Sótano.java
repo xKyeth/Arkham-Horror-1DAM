f0f5a45a38cfee8448723abf9c88d0108b26605d
usuario <usuario@usuario-OptiPlex-3050>
2019-03-12 12:02:41 +0100

    Modificada la herencia de la clase Carta

--- src/Lugar/Sótano.java
+++ src/Lugar/Sótano.java
@@ -4,12 +4,16 @@ package Lugar;
 public class Sótano extends Lugar{
     
     public Sótano(){
-        super.nombre = "Sótano";
+        super("Sótano",false);
         super.descripcionNR = "Las escaleras que bajan al sótano están resbaladizas y brillan con una fina capa de hielo…";
         super.descripcionR = "El sótano parece haber quedado sustituido por una red subterránea de helados túneles y cavernas. El frío os hiela hasta el tuétano.";
         super.velo = 4;
         super.pistas = 2;
         super.obligado = "**Después de que entres en el Sótano: Recibe 1 punto de daño.**";
+    }
+
+    @Override
+    public void Accion() {
     }
     
 }
