5ab0c94a936bb5a43d0acc781e11084b85289afe
Imanol G <Imanol G@DESKTOP-3HRN8AN>
2019-03-13 13:30:21 +0100

    Mazo plan version 3.0

---
+++ src/Mazo_Plan/MazoPlan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mazo_Plan;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author Imanol G
+ */
+public class MazoPlan {
+    
+    ArrayList<CartaPlan> cartasPlan = new ArrayList<>();
+    
+    public void CartasPlan(){
+       
+       
+       
+   }
+    
+    
+}
