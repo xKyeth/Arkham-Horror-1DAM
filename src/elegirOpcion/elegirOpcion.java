6305b0b874ee6158e74958ee692aeac97f9a0d5f
usuario <usuario@usuario-ALDA>
2019-03-13 12:59:07 +0100

    Policia Raso

--- src/elegirOpcion/elegirOpcion.java
+++ src/elegirOpcion/elegirOpcion.java
@@ -3,22 +3,37 @@ package elegirOpcion;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class elegirOpcion {
-    ArrayList opciones = new ArrayList();
+    ArrayList <Integer> opciones = new ArrayList();
     Scanner sc = new Scanner(System.in);
+    //Añades el número de la opción que se ha elegido al ArrayList de opciones.
+    //Ejemplo:
+    // System.out.println("1. Opcion 1");
+    // System.out.println("2. Opcion 2");
+    //int y = scanner.nextInt();
+    //switch (y){
+    //  case 1: otrometodocualquiera();
+        //      addOpcion(1);break;
+    //  case 2: otrometodocualquiera2();
+        //      addOpcion(2);break}
+    
     public void addOpcion(int x){
         opciones.add(x);     
     }
     
     public int seleccionarOpcion(){
+        //Primero muestra todas las opciones que has elegido.
         System.out.println("Tus opciones elegidas son: ");
         for (int i = 0; i < opciones.size(); i++) {
             System.out.print(" "+opciones.get(i)+" ");
                   
         }
+        //Luego se selecciona la opcion que se desea realizar (Por ejemplo si
+        // al leer el ArrayList pone las opciones (3 5) si introduces el numero
+        //2 sacará la opción 5)
         System.out.println("Introduce la opcion que quieres realizar: ");
         int op = sc.nextInt();
-        opciones.get(op);
-        return op;
+        int i = opciones.get(op);
+        return i;
         
     }
     
