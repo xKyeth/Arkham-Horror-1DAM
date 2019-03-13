ac99f1363460e85610217d1ade6d8bbd9909fa77
usuario <usuario@usuario-ALDA>
2019-03-13 11:59:48 +0100

    nueva carta traicion

---
+++ src/Modelo/VocesDisonantes.java
@@ -0,0 +1,12 @@
+
+package Modelo;
+
+import CartasMito.Traicion;
+
+public class VocesDisonantes extends Traicion{
+    
+    public VocesDisonantes() {
+        super("Voces Disonantes", false);
+    }
+    
+}
