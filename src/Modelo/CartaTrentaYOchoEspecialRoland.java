cd744919866305a5c66bf39af88b82f13f4c69be
Miguel <Miguel@192.168.1.3>
2019-03-16 18:45:04 +0100

    Añadida la prueba de combate (comentada) a modelohabilidad, me faltan los enemigos que no se ahora mismo de donde sacarlos, lo demás creo que todo ok

--- src/Modelo/CartaTrentaYOchoEspecialRoland.java
+++ src/Modelo/CartaTrentaYOchoEspecialRoland.java
@@ -4,28 +4,46 @@
  * and open the template in the editor.
  */
 package Modelo;
+import CartasInvestigador.CartasInvestigador;
 import Lugar.Lugar;
 import Modelo.RolandBanks;
 /**
  *
  * @author miguel
  */
-public class CartaTrentaYOchoEspecialRoland extends Carta{
+public class CartaTrentaYOchoEspecialRoland extends CartasInvestigador{
         RolandBanks inv;
         Enemigo ene;
+        Exception exception;
       private  int usos;
-    public CartaTrentaYOchoEspecialRoland() {
-      super("38 Especial de Roland", "Neutral", 3, "Arma", 1, 0, 0, 1, false, "Arma de fuego");
-      usos=4;
+    public CartaTrentaYOchoEspecialRoland(int fichaPerdicion,int suministro) {
+        
+        super(".38 Especial De Roland", true, fichaPerdicion, 0, 0, 1, 1, 1, 0, 0, 3, suministro, true);
     }
-    public void usaCarta(){
-       
-        if(inv.getLugar().getPistas()>0){
-            ene.setVida(ene.getVida()-(inv.getDaño()+3));
-        }else{
-            ene.setVida(ene.getVida()-(inv.getDaño()+1));
-        }
-        usos--;
+
+    public void usaCarta(Enemigo e){
+       try{
+          
+        if(usos>0){
+            if(inv.getLugar().getPistas()>0){
+                e.setVida(e.getVida()-(inv.getDaño()+3));
+            }else{
+                e.setVida(e.getVida()-(inv.getDaño()+1));
+            }
+            usos--;
+       }else{
+           throw exception= new Exception("No te quedan balas");
+           
+       }
+       }catch(Exception ex){
+           ex= new Exception("El enemigo no está en tu mismo lugar");
+       }
+    }
+
+    @Override
+    public void verCarta() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
+
