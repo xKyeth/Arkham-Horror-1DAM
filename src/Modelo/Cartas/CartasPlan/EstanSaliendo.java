bf121252ce26f7a4839301da7cbe565e1d2dfae0
Imanol G <Imanol G@DESKTOP-3HRN8AN>
2019-03-18 10:15:14 +0100

    Mazo plan al 90% me falta lo que esta comentado en la clase EstanSaliendo

--- src/Modelo/Cartas/CartasPlan/EstanSaliendo.java
+++ src/Modelo/Cartas/CartasPlan/EstanSaliendo.java
@@ -22,7 +22,25 @@ public abstract class EstanSaliendo extends CartaPlan {
     //es el pasillo o la salita y como saber cuandop termina la ronda.
     
     
+    public EstanSaliendo(){
+        cp.setTitulo("EstanSaliendo");
+        
+        cp.setTexto("Oís un demencial aullido en el exterior, y d epronto todas"
+                + " las criaturas vuelven su atención a ese sonido. Corren para"
+                + " escapar de la casa, derribando puertas y desgarrando cuanto"
+                + " encuentran en su camino.");
+        
+        cp.setAccion("El suelo comienza a temblar, y veis garras que salen de la"
+                + " tierra mientras las imposibles criaturas tratan desesperadamente"
+                + " de cavar para emerger del suelo que os rodea. Dondequiera que "
+                + "miréis hay monstruos saliendo de la tierra. Aterrados, huis tan rápido como podéis.");
+        
+        
+        cp.setPerdicion(10);
+    }
+    
     public void pasivaCarta(){
+        
         
         //Al final de la fase enemigos los GUL se mueven 1 lugar hacia el investigador
         //Al final de cada ronda se suma 1 ficha de perdicion por cada GUL que haya en el pasillo o salita
@@ -34,7 +52,8 @@ public abstract class EstanSaliendo extends CartaPlan {
     public void usarCarta(){
         
         
-        
+        if( cp.getPerdicionEnJuego() >= cp.getPerdicion()){
+
         if(ca.getTitulo()=="Atrapados" || ca.getTitulo()=="La barrera"){
             
             //GameOver
@@ -49,5 +68,6 @@ public abstract class EstanSaliendo extends CartaPlan {
         
     }
     
-    
+    }
 }
+
