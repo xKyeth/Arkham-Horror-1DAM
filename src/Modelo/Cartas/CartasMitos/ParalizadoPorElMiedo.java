b65f2c1cae870c9879c46cf06954c0805b024c4a
pecorte <pecorte@yahoo.es>
2019-03-20 11:29:01 +0100

    Cambios

--- src/Modelo/Cartas/CartasMitos/ParalizadoPorElMiedo.java
+++ src/Modelo/Cartas/CartasMitos/ParalizadoPorElMiedo.java
@@ -12,17 +12,10 @@ import Modelo.Investigador;
  * @author pecorte
  */
 public class ParalizadoPorElMiedo extends Traicion {
-    String accion="La primera vez que realices una de las siguientes acciones (moverte, combatir o evitar) cada ronda, te cuesta 1 acción adicional.\n" +
-                    "\n" +
-                    "Obligado - Al final de tu turno: Realiza una prueba de  (3). Si tienes éxito, descarta Paralizado por el miedo.";
     boolean activo=false;
 
     public ParalizadoPorElMiedo(String nombreCarta, boolean preparada, int fichaPerdicion) {
         super("Paralizado por el miedo", false, 0);
-    }
-
-    public String getAccion() {
-        return accion;
     }
     
     public void activarCarta(){
@@ -40,6 +33,13 @@ public class ParalizadoPorElMiedo extends Traicion {
     @Override
     public void Accion(Investigador investigador) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void verCarta() {
+        System.out.println("La primera vez que realices una de las siguientes acciones (moverte, combatir o evitar) cada ronda, te cuesta 1 acción adicional.\n" +
+                    "\n" +
+                    "Obligado - Al final de tu turno: Realiza una prueba de  (3). Si tienes éxito, descarta Paralizado por el miedo.");
     }
     
 }
